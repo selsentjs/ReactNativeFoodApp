4fafb20d39c21254ae2872608acf6f4a05f0aa9d
selvisentest2006 <selvisentest2006@gmail.com>
2024-04-13 09:15:44 +0530

    change get by id

--- backend/controllers/gridFoodController.js
+++ backend/controllers/gridFoodController.js
@@ -1,5 +1,5 @@
 const GridFood = require('../models/GridFood');
-//const data = require('./gridData.json');
+const data = require('./gridImage.json');
 
 // get all the foods
 const getAllFoods = async (req, res) => {
@@ -14,9 +14,11 @@ const getAllFoods = async (req, res) => {
 
 // get single food
 const getSingleFood = async (req, res) => {
-  const {id: id} = req.params;
+  const {id} = req.params;
   try {
-    const food = await GridFood.findOne({_id: id});
+    const food = data.food.find(item => item._id === id); // Search for the food item by ID
+    // const food = await GridFood.findOne({_id: id});
+
     res.status(200).json({food});
   } catch (err) {
     res.status(500).json(err);
